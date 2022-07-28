3015af68ec17877d01137fda2b8586d2c1cf9b88
hann0r <92484605+hann0r@users.noreply.github.com>
2022-07-28 17:28:47 +0100

    Added firebase config

--- index.tsx
+++ index.tsx
@@ -1,11 +1,25 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-import './index.css';
-import App from './App';
+import reportWebVitals from "./reportWebVitals";
 
-import reportWebVitals from './reportWebVitals';
+const firebaseConfig = {
+  apiKey: "AIzaSyCusVBw4V8t0__A-52qnweaDpcuXGs34Fo",
+  authDomain: "to-do-app-56d9a.firebaseapp.com",
+  projectId: "to-do-app-56d9a",
+  storageBucket: "to-do-app-56d9a.appspot.com",
+  messagingSenderId: "977469462064",
+  appId: "1:977469462064:web:1c735e34ebb8ebf50dbff5",
+  measurementId: "G-JMLV3PS4SH",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +27,7 @@ ReactDOM.render(
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
